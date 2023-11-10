0e7b828204ea40d40cbedb2f03d462739cbbd6b5
JADEN <josekorea1996@gmail.com>
2023-11-09 18:06:47 -0600

    Proyecto ClubGeek ecommerce

---
+++ Controllers/productos-controller.js
@@ -0,0 +1,32 @@
+
+const crearNuevoProducto = (imageUrl, name, price, id) => {
+    const cardProducto = document.createElement("div")
+    cardProducto.classList.add("producto")
+    const contenido = `
+        
+    <img src="${imageUrl}" alt="img">
+    <h1 class="product-name"> ${name} </h1>
+    <p class="precio">${price}</p>
+    <button class="btn">Ver Producto<a class="ver-producto" href="#"></a></button>
+    
+    `;
+    cardProducto.innerHTML = contenido;
+    return cardProducto;
+   
+
+}
+
+const seccionProductos = document.querySelector('[data-product="productos"]');
+
+const listaProductos = () => {
+    return fetch("http://localhost:3000/producto").then((respuesta) => respuesta.json()
+    );  
+}
+
+listaProductos().then((data) =>{
+    data.forEach(product => {
+        const nuevoProducto = crearNuevoProducto(product.imageUrl, product.name, product.price);
+        seccionProductos.appendChild(nuevoProducto)
+     });
+}).catch((error) => alert("Ocurrio un error"))
+
